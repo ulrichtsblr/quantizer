1fc48ed7ec29e3147608180b5944dc6252367af3
ulrichtsblr <ulrichts.blr@protonmail.com>
2021-12-02 19:28:48 +0100

    refactor controller

---
+++ src/test/python/test_quantizer/test_controller.py
@@ -0,0 +1,11 @@
+from quantizer import ArbitraryController
+import unittest
+
+
+class TestController(unittest.TestCase):
+    def test(self):
+        ArbitraryController()
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/tree/python/quantizer/controller.py
+++ src/tree/python/quantizer/controller.py
@@ -1,21 +1,3 @@
-from quantizer.kernel.context import Controller
+from quantizer.kernel.waveform import Controller
 
-
-class UnipolarController(Controller):
-    pass
-
-
-class BipolarController(Controller):
-    pass
-
-
-class StaticController(Controller):
-    def __init__(self, scalar):
-        """
-        :param scalar: scalar
-        :type scalar: Scalar
-        :rtype None
-        """
-        super().__init__(
-            Controller.cast(scalar).get_ndarray()
-        )
+ArbitraryController = Controller
