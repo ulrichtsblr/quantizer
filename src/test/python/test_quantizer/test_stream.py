b37ba975d15ef0e8381fb28239623988aacc0f2f
ulrichtsblr <ulrichts.blr@protonmail.com>
2021-12-02 19:29:16 +0100

    refactor stream

---
+++ src/test/python/test_quantizer/test_stream.py
@@ -0,0 +1,11 @@
+from quantizer import ArbitraryStream
+import unittest
+
+
+class TestStream(unittest.TestCase):
+    def test(self):
+        ArbitraryStream()
+
+
+if __name__ == "__main__":
+    unittest.main()
